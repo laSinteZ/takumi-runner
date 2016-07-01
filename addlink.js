22276aaa2ca8c337e08b0be607214ac5f54e8bfa
Vyacheslav Bikbaev <la.sintez@gmail.com>
2016-07-01 06:16:14 +0300

    Initial commit

---
+++ addlink.js
@@ -0,0 +1,19 @@
+function addLinks() {
+
+    var vkLink;
+    var listens = document.getElementsByClassName("listen");
+    for (i = 0; i < listens.length; i++) {
+
+        authorName = listens[i].lastElementChild.getAttribute("data-artist");
+        songName = listens[i].lastElementChild.getAttribute("data-name");
+
+        vkLink = document.createElement('a');
+        vkLink.setAttribute("href", "https://vk.com/audio?q=" + authorName + "%20" + songName);
+        vkLink.setAttribute("target", "_blank"),
+        vkLink.innerHTML = "VK";
+
+        listens[i].appendChild(vkLink);
+    }
+}
+
+addLinks();
